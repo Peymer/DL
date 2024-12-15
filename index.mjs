dbc3472034743c7a32ffbc4ab1794c0ad453a791
Igor Peymer <ipigp@yandex.ru>
2024-12-15 15:49:39 +0300

    first 	new file:   .gitignore 	new file:   courses/404.html 	renamed:    curses/js-debug/README.md -> courses/js-debug/README.md 	renamed:    curses/js-debug/black.css -> courses/js-debug/black.css 	renamed:    curses/js-debug/highlight.esm.js -> courses/js-debug/highlight.esm.js 	renamed:    curses/js-debug/highlight.js -> courses/js-debug/highlight.js 	renamed:    curses/js-debug/index.html -> courses/js-debug/index.html 	renamed:    curses/js-debug/monokai.css -> courses/js-debug/monokai.css 	renamed:    curses/js-debug/notes.js -> courses/js-debug/notes.js 	renamed:    curses/js-debug/pic/1.png -> courses/js-debug/pic/1.png 	renamed:    curses/js-debug/pic/2.png -> courses/js-debug/pic/2.png 	renamed:    curses/js-debug/pic/3.png -> courses/js-debug/pic/3.png 	renamed:    curses/js-debug/pic/4.png -> courses/js-debug/pic/4.png 	renamed:    curses/js-debug/pic/5.png -> courses/js-debug/pic/5.png 	renamed:    curses/js-debug/pic/chrome.png -> courses/js-debug/pic/chrome.png 	renamed:    curses/js-debug/plugin.js -> courses/js-debug/plugin.js 	renamed:    curses/js-debug/reveal.css -> courses/js-debug/reveal.css 	renamed:    curses/js-debug/reveal.js -> courses/js-debug/reveal.js 	renamed:    curses/js-debug/zenburn.css -> courses/js-debug/zenburn.css 	renamed:    curses/mobirise/README.md -> courses/mobirise/README.md 	renamed:    curses/mobirise/black.css -> courses/mobirise/black.css 	renamed:    curses/mobirise/index.html -> courses/mobirise/index.html 	renamed:    curses/mobirise/mobirise/2dee548419c1f6158188151ab811c383.jpg -> courses/mobirise/mobirise/2dee548419c1f6158188151ab811c383.jpg 	renamed:    curses/mobirise/mobirise/4540d8b0e925cf9427bd1e9a2b723e56.jpg -> courses/mobirise/mobirise/4540d8b0e925cf9427bd1e9a2b723e56.jpg 	renamed:    curses/mobirise/mobirise/511d6bda37a62e6f423be15664570677.jpg -> courses/mobirise/mobirise/511d6bda37a62e6f423be15664570677.jpg 	renamed:    curses/mobirise/mobirise/511db6177199bc80c21b078f83c415dd.jpg -> courses/mobirise/mobirise/511db6177199bc80c21b078f83c415dd.jpg 	renamed:    curses/mobirise/mobirise/Mobirise_redactor.jpg -> courses/mobirise/mobirise/Mobirise_redactor.jpg 	renamed:    curses/mobirise/mobirise/Mobirise_settings.webp -> courses/mobirise/mobirise/Mobirise_settings.webp 	renamed:    curses/mobirise/mobirise/Mobirise_shablon.webp -> courses/mobirise/mobirise/Mobirise_shablon.webp 	renamed:    curses/mobirise/mobirise/d7e62845538f42a6a994782c56d1829e.jpg -> courses/mobirise/mobirise/d7e62845538f42a6a994782c56d1829e.jpg 	renamed:    curses/mobirise/notes.js -> courses/mobirise/notes.js 	renamed:    curses/mobirise/reveal.css -> courses/mobirise/reveal.css 	renamed:    curses/mobirise/reveal.js -> courses/mobirise/reveal.js 	new file:   index.mjs 	new file:   package-lock.json 	new file:   package.json

---
+++ index.mjs
@@ -0,0 +1,54 @@
+import F from 'fastify'
+import fs from '@fastify/static'
+import path from 'node:path'
+import emiter from 'node:events'
+
+const __dirname=import.meta.dirname
+emiter.setMaxListeners(12); // боремся предупреждением об утечке памяти
+
+let srv=F({ logger: true });
+
+srv.register(fs,{
+    root: path.join(__dirname,'courses'),
+    // prefix: '/courses/',
+    prefix: '/',
+          // html or json response? html requires a render method.
+        //   format: 'html',
+          // A list of filenames that trigger a directory list response.
+          names: ['index', 'index.html', 'index.htm', '/'],
+});
+
+// srv.register(fs,{
+//     root: path.join(__dirname,'node_modules'),
+//     prefix: '/node_modules/',
+//     decorateReply: false
+// });
+
+srv.get('/:name', async (q,a)=>{
+    let name=q.params.name;
+    console.log(`Get: ${name}`);
+    return a.sendFile(name);
+})
+
+srv.setNotFoundHandler(async (q,a)=>{
+    console.log(`GET: ${q.params.name} ${q.url} not found`);
+    return a.sendFile("404.html")
+   // let p = path.join(__dirname, 'curses', q.url, 'index.html');
+   // return a.sendFile(p);
+})
+
+const start=async ()=>{  
+try {
+     await srv.listen({port:3000, host: '0.0.0.0'}, async (err, addr)=>{
+        if (err)
+            console.log(err);
+        else
+            console.log(`listern ${addr}`);
+    });
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+start();
